01e563332101ee1e4be0b4e3325594c2d112aca4
Elizaveta Lvova-Kirisik <e.kirisik@yandex.ru>
2022-08-03 14:20:01 +0300

    add Example005_IfElse with .ToLower

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите имя пользователя: ");
+string userName = Console.ReadLine ();
+if(userName.ToLower() == "маша")
+{
+    Console.WriteLine ("Ура, это же МАША!");
+}
+else
+{
+    Console.Write ("Добро пожаловать, ");
+    Console.Write (userName);
+};
