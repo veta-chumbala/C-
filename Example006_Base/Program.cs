2fd2e9edd33a8cfa139ab044857e741969587dd1
Elizaveta Lvova-Kirisik <e.kirisik@yandex.ru>
2022-08-03 15:27:25 +0300

    что-то изменилось, не заметила что

--- Example006_Base/Program.cs
+++ Example006_Base/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 1;
+int b = 8;
+int c = 3;
+int d = 2;
+int e = 6;
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+{
+    Console.Write ("max = ");
+    Console.Write (max);
+};
